906278049b09f0a851fe4dbe65cd1c9480caeb0e
”MrQin” <13279231302@163.com>
2022-10-09 20:06:35 +0800

    add  LeetCode 160、21、86、142

--- src/list/operateList.py
+++ src/list/operateList.py
@@ -1,6 +1,10 @@
 from asyncio.log import logger
+from copy import copy
+from email.errors import HeaderParseError
 from email.quoprimime import header_check
+from inspect import isfunction
 import logging
+import re
 from typing import List
 from .creatList import ListNode
 from src.common.logger import Logger
@@ -63,3 +67,75 @@ class OperateList(object):
         head.next.next = head
         head.next = None
         return newhead
+
+    def intersect_list(self, haedA: ListNode, headB: ListNode) -> ListNode:
+        """
+        find intersect list
+        """
+        if haedA == None and headB == None:
+            return None
+        copy_haedA = haedA
+        copy_haedB = headB
+        while copy_haedA != copy_haedA:
+            if copy_haedA == None:
+                copy_haedA = headB
+            else:
+                copy_haedA = copy_haedA.next
+            if copy_haedB == None:
+                copy_haedB = haedA
+            else:
+                copy_haedB = copy_haedB.next
+        return copy_haedA
+
+    def merge_2_list(self, headA: ListNode, headB: ListNode):
+        """
+        merge two ascending list
+        """
+        #iteration
+        #time:O(m+n),space:O(1)
+        final_list = ListNode(-1)
+        tmp_list = final_list
+        while headA and headB:
+            if headA.val > headB.val:
+                tmp_list.next = headB
+                headB = headB.next
+            else:
+                tmp_list.next = headA
+                headA = headA.next
+            tmp_list = tmp_list.next
+        tmp_list.next = headB if headA == None else headA
+        return final_list
+        # #recursion
+        # #time:O(m+n),space:O(m+n)
+        # if headA == None:
+        #     return headB
+        # if headB == None:
+        #     return headA
+        # if headA.val > headB.val:
+        #     headB.next = self.merge_2_list(headA, headB.next)
+        #     return headB
+        # else:
+        #     headA.next = self.merge_2_list(headA.next, headB)
+        #     return headA
+        
+    def partition_list(self, head: ListNode, x: int):
+        """
+        partition list,less then x in front
+        """
+        if head == None:
+            return None
+        headA = ListNode
+        headB = ListNode
+        tmp_headA = headA
+        tmp_headB = headB
+        while head != None:
+            if head.val < x:
+                tmp_headA.next = head
+                tmp_headA = tmp_headA.next
+            else:
+                tmp_headB.next = head
+                tmp_headB = tmp_headB.next
+            head = head.next
+        tmp_headB.next = None
+        tmp_headA.next = headB
+        return headA
